858608b0863356a2fd908279e077a329b6d8b455
dmoni <dmoni@DHAMONI>
2023-09-27 16:30:53 +0530

    created orders page

--- src/test/java/com/ecom/Essen_Rad_GitHub/DemoTest.java
+++ src/test/java/com/ecom/Essen_Rad_GitHub/DemoTest.java
@@ -1,5 +1,12 @@
 package com.ecom.Essen_Rad_GitHub;
 
+import org.testng.annotations.Test;
+
 public class DemoTest {
+	@Test
+	public void demomethod()
+	{
+		System.out.println("--- Test script 1 ---");
+	}
 
 }
