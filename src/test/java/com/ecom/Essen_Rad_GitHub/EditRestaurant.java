5b91d32fb3307c273b22db4104a05f606b675ce6
HI <HI@DESKTOP-8ELVUAA>
2023-09-27 17:12:31 +0530

    EditedRestaurant

---
+++ src/test/java/com/ecom/Essen_Rad_GitHub/EditRestaurant.java
@@ -0,0 +1,12 @@
+package com.ecom.Essen_Rad_GitHub;
+
+import org.testng.annotations.Test;
+
+public class EditRestaurant {
+	@Test
+	public void editRestaurant()
+	{
+	System.out.println("Restaurant edited successfully");	
+	}
+
+}
