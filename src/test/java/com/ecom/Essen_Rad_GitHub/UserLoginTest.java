dae3efba1811f3f647434d7f4c42b1217e5213c8
dmoni <dmoni@DHAMONI>
2023-09-28 10:57:36 +0530

    user logged in

---
+++ src/test/java/com/ecom/Essen_Rad_GitHub/UserLoginTest.java
@@ -0,0 +1,12 @@
+package com.ecom.Essen_Rad_GitHub;
+
+import org.testng.annotations.Test;
+
+public class UserLoginTest {
+	@Test
+	public void userLogin()
+	{
+		System.out.println("--Test script--");
+	}
+
+}
